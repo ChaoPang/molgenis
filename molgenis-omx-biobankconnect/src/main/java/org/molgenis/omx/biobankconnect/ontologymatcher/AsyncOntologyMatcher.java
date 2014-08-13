ddc0c9b15125286f2166e4853eb06993f0e2d1eb
Chao Pang <ChaoPang229@gmail.com>
2014-08-13 14:23:41 +0200

    removed the limit of number of data elements can be retrieved

--- molgenis-omx-biobankconnect/src/main/java/org/molgenis/omx/biobankconnect/ontologymatcher/AsyncOntologyMatcher.java
+++ molgenis-omx-biobankconnect/src/main/java/org/molgenis/omx/biobankconnect/ontologymatcher/AsyncOntologyMatcher.java
@@ -508,7 +508,7 @@ public class AsyncOntologyMatcher implements OntologyMatcher, InitializingBean
 		{
 			List<QueryRule> finalQuery = new ArrayList<QueryRule>();
 			finalQuery.add(disMaxQuery);
-			finalQuery.add(new QueryRule(Operator.LIMIT, 50));
+			finalQuery.add(new QueryRule(Operator.LIMIT, Integer.MAX_VALUE));
 			MultiSearchRequest request = new MultiSearchRequest(Arrays.asList(CATALOGUE_PREFIX + dataSetId,
 					FEATURE_CATEGORY + dataSetId), finalQuery, null);
 			result = searchService.multiSearch(request);
